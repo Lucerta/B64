dc43141d5327da80f74ae8665f55a8d0e68c1078
Lucerta <dvdcppl@gmail.com>
2022-01-18 16:19:41 -0500

    Commit

--- src/com/lucerta/base64/B64Converter.java
+++ src/com/lucerta/base64/B64Converter.java
@@ -109,8 +109,7 @@ public class B64Converter
             int b = REVERSE_LOOKUP[c];
             if (b == -2) continue; // b = 9 (tab), 10 (lf), 13 (cr) or 32 (spc)
             if (ending || b == -1)
-                throw new IllegalArgumentException(
-                    "Invalid char val=" + (int)c);
+                throw new IllegalArgumentException("Invalid char val=" + (int)c);
             buffer.write((byte)b);
         }
         return buffer.toByteArray();
