fd79fb05be4e9e752a1430de69192d9b5aada4d0
Owner <Owner@email.com>
2022-01-20 00:29:23 -0500

    Commit

--- src/com/lucerta/base64/B64Converter.java
+++ src/com/lucerta/base64/B64Converter.java
@@ -98,19 +98,19 @@ public class B64Converter
     {
         ByteArrayOutputStream buffer = new ByteArrayOutputStream();
         boolean ending = false;
-        for (byte datum : data)
+        for (byte b : data)
         {
-            char c = (char)datum;
+            char c = (char)b;
             if (c == '=')
             {
                 ending = true;
                 continue;
             }
-            int b = REVERSE_LOOKUP[c];
-            if (b == -2) continue; // b = 9 (tab), 10 (lf), 13 (cr) or 32 (spc)
-            if (ending || b == -1)
+            int index = REVERSE_LOOKUP[c];
+            if (index == -2) continue; // b = 9 (tab), 10 (lf), 13 (cr) or 32 (spc)
+            if (ending || index == -1)
                 throw new IllegalArgumentException("Invalid char val=" + (int)c);
-            buffer.write((byte)b);
+            buffer.write((byte)index);
         }
         return buffer.toByteArray();
     }
